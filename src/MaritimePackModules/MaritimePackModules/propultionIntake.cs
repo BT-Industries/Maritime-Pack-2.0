e1e3e65cf25feb448f0416c0baa1df1f7b9fb4d2
Clifton M <Clifton@Cliftons-MacBook.local>
2016-04-30 21:11:43 -0400

    Update engines, add part module

---
+++ src/MaritimePackModules/MaritimePackModules/propultionIntake.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MaritimePackModules
+{
+	public class propultionIntake :PartModule
+	{
+
+		[KSPField]
+		public string ResourceName = "";
+
+		void Update () {
+
+			var resourceList = part.Resources.list;
+
+			foreach (PartResource resource in resourceList) {
+
+				if (resource.resourceName == ResourceName) {
+
+					vessel.checkSplashed ();
+
+					if (resource != null && vessel.Splashed) {
+
+						resource.amount = resource.maxAmount;
+
+					}
+
+				}
+
+			}
+
+		}
+
+	}
+}
+
